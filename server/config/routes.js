e9693febe1ba795698005476d5df44f56e684649
Dieiverson Kava <38850216+Dieiverson@users.noreply.github.com>
2020-11-26 19:01:12 -0300

    Ajustes para ficar de acordo com a prova 2b

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,12 +2,6 @@ const express = require("express");
 const router = express.Router();
 const MedicoController = require("../controllers/MedicoController.js");
 
-
-//router.get("/", usuarioController.Get);
 router.post("/medico/cadastrar", MedicoController.Cadastrar);
-router.get("/medico/buscar/:crm", MedicoController.GetMedicoByCrm);
 router.get("/medico/listar", MedicoController.Get);
-router.post("/medico/alterar", MedicoController.Alterar);
-router.get("/medico/remover/:crm", MedicoController.Delete);
-
 module.exports = router;
--- server/controllers/MedicoController.js
+++ server/controllers/MedicoController.js
@@ -3,7 +3,7 @@ const medico = require("../models/MedicoSchema.js");
 class MedicoController {
 
     async Cadastrar(req, res) {
-        var crmMedico = req.body.crm;       
+        var crmMedico = req.body.crm;
         var user = await medico.find({ crm: crmMedico });
         if (user[0] != null) {
             console.log("Não inseriu");
@@ -14,40 +14,8 @@ class MedicoController {
             return res.status(200).json(resultado);
         }
     }
-
-    async Alterar(req, res) {
-        var medicoUser = req.body;
-        console.log( req.body);
-        const update = {
-            "$set": {
-              "nome": medicoUser.nome,
-              "crm": medicoUser.crm,
-              "especialidade":medicoUser.especialidade
-            }
-        };  
-       
-        var resultado = await medico.findOneAndUpdate({ crm: medicoUser.crm },update);
-        return res.status(200).json(resultado);
-        
-    }
-
     async Get(req, res) {
         var result = await medico.find({}); //Find sem parâmetro para trazer todos
-        res.status(200).json(result);
-    }
-    async Delete(req, res) {
-        var crmMedico = req.params.crm;
-        var result = await medico.findOneAndRemove({crm: crmMedico });
-        res.status(200).json(result);
-    }
-
-    async GetMedicoByCrm(req, res) {
-        var crmRecebido = req.params.crm;
-        var result = await medico.find( {crm : crmRecebido },
-            function(err, doc) {
-                if (err)
-                    return res.send(500, { error: err });
-            });
         res.status(200).json(result);
     }
 }
